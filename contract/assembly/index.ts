f05fdf7e3972a0dec476581af2dce3dbf16e0373
Develop <2342342342342@234234232322.com>
2021-05-07 00:02:34 -0700

    isValidApprover, authorizeMission working, but NOT isMissionAuthorized

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,40 +1,32 @@
-/*
- * This is an example of an AssemblyScript smart contract with two simple,
- * symmetric functions:
- *
- * 1. setGreeting: accepts a greeting, such as "howdy", and records it for the
- *    user (account_id) who sent the request
- * 2. getGreeting: accepts an account_id and returns the greeting saved for it,
- *    defaulting to "Hello"
- *
- * Learn more about writing NEAR smart contracts with AssemblyScript:
- * https://docs.near.org/docs/roles/developer/contracts/assemblyscript
- *
- */
+import { PersistentMap, PersistentVector, u128, PersistentSet, storage, base58, env, Context, logging } from "near-sdk-as";
 
-import { Context, logging, storage } from 'near-sdk-as'
+let validApproverSet=new PersistentSet<string>('approvers')
+validApproverSet.add('axle.testnet')
+validApproverSet.add('fdsfds.testnet');
 
-const DEFAULT_MESSAGE = 'Hello'
+let missionApprovalMap = new PersistentMap<string, bool>("missionApprovals");
 
-// Exported functions will be part of the public interface for your smart contract.
-// Feel free to extract behavior to non-exported functions!
-export function getGreeting(accountId: string): string | null {
-  // This uses raw `storage.get`, a low-level way to interact with on-chain
-  // storage for simple contracts.
-  // If you have something more complex, check out persistent collections:
-  // https://docs.near.org/docs/roles/developer/contracts/assemblyscript#imports
-  return storage.get<string>(accountId, DEFAULT_MESSAGE)
+export function isValidApprover():bool{
+    logging.log(Context.sender);
+    return validApproverSet.has(Context.sender)
 }
 
-export function setGreeting(message: string): void {
-  const account_id = Context.sender
+export function authorizeMission(missionInt:i32, authorizedValue:bool=true):bool{
+    let key = Context.sender+missionInt.toString();
+    missionApprovalMap.set(key, authorizedValue);
+    logging.log(missionApprovalMap);
+    return missionApprovalMap.getSome(key);
+}
 
-  // Use logging.log to record logs permanently to the blockchain!
-  logging.log(
-    // String interpolation (`like ${this}`) is a work in progress:
-    // https://github.com/AssemblyScript/assemblyscript/pull/1115
-    'Saving greeting "' + message + '" for account "' + account_id + '"'
-  )
-
-  storage.set(account_id, message)
+export function isMissionAuthorized(missionInt:i32):bool{
+    let returnValue=true;
+    validApproverSet.values().forEach((approverString: string) => {
+        logging.log(approverString);
+        // let key = approverString+missionInt.toString();
+        // let isApproved=missionApprovalMap.get(key);
+        // if(!=true){
+        //     returnValue=false;
+        // }
+    });
+    return returnValue;
 }
